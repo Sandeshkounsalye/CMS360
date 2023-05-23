1f629494fab5e6f36457b010f36e14581952b038
Sandesh <sandeshpk411@gmail.com>
2023-05-23 09:00:04 +0530

    Initial Commit

---
+++ cms360/src/main/java/com/example/cms360.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class cms360 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(cms360.class, args);
+	}
+
+}
